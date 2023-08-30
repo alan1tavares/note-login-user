a722948253d771088968491fa8350b1d8966f8a6
Alan Tavares <alan1tavares@gmail.com>
2023-08-30 12:12:26 -0300

    feat: add interface repository async

--- Domain/Entity/IEntity.cs
+++ Domain/Entity/IEntity.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Entity
+﻿namespace Domain.Entity
 {
-    internal interface IEntity
+    public interface IEntity
     {
         public Guid Id { get; set; }
     }
---
+++ Domain/UseCase/IRepositoryAsync.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entity;
+using Domain.UseCase.Result;
+
+namespace Domain.UseCases
+{
+    public interface IRepositoryAsync<TEntity> where TEntity : class, IEntity
+    {
+        Task<NoteResult> InsertAsync(TEntity entity);
+        Task<NoteResult> UpdateAsync(TEntity entity);
+        Task<NoteResult> RemoveAsync(Guid id);
+        Task<TEntity> GetById(Guid id);
+        Task<IEnumerable<TEntity>> GetAll();
+    }
+}
